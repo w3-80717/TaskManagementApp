8a4ead0d58bd0207501ef12fd3180cc551e9a94d
w3-80717 <chavan1609@gmail.com>
2024-09-29 20:40:09 +0530

    build updated

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+echo "create public directory"
 mkdir -p backend/public
 
+echo "go to front end"
 cd frontend
+
+echo "install front end deps"
 npm i
+
+echo "build front end"
+
 npm run build
+
+echo "move build"
 mv ./dist/* ../backend/public/
-cd backend
+
+echo "go to backend"
+cd ../backend
 npm i
