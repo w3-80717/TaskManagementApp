b0e3fbcc8c34b0ff183957fa0536b1adf1403865
w3-80717 <chavan1609@gmail.com>
2024-09-29 19:52:43 +0530

    build frontend script

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,6 @@ app.use(bodyParser.json());
 connectDB();
 
 app.use('/api/tasks', taskRoutes);
-
+app.use(express.static("./public"))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
